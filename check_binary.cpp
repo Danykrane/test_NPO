1d02e75c289fcb909c8928a4fcd00b364bf84016
Danykrane <gvg5555@gmail.com>
2022-10-29 13:04:59 +0300

    test_str
    тестирую бинарник

--- check_binary.cpp
+++ check_binary.cpp
@@ -6,6 +6,22 @@
 
 using namespace std;
 
+void readf(string &, string &); //для работы через консоль (вводим пути к папкам)
+void check_wr(string &);        // при вставке папки в vscode появляются '/Users/artemgudzenko/Desktop'
+string inspect(string &);
+void st_chage(string &, string &);
+
+int main()
+{
+    string stri, stro;
+    // readf(stri, stro);
+    // check_wr(stri);
+    // check_wr(stro);
+    stri = "/Users/artemgudzenko/Desktop/Less_1/c++/test_NPO/input";
+    stro = "/Users/artemgudzenko/Desktop/Less_1/c++/test_NPO/ouutput";
+    st_chage(stri, stro);
+}
+
 void readf(string &stri, string &stro) //для работы через консоль (вводим пути к папкам)
 {
     cout << "Введите пусть к папке исходного файла\n=> ";
@@ -22,13 +38,11 @@ void check_wr(string &str1) // при вставке папки в vscode поя
         str1.erase(remove(str1.begin(), str1.end(), '\''), str1.end());
 }
 
-string inspect(string &);
-
-void test(string &stri, string &stro)
+void st_chage(string &stri, string &stro)
 {
     cout << "Открытие файла и его редактирование" << endl;
     stri += "/acsii_tmi.txt";
-    stro += "/out.txt";
+    stro += "/out.bin";
     ifstream file(stri);                          //на чтение (исходный каталог)
     ofstream mfile(stro, ios::binary | ios::out); //на запись (выходной каталог)
     string str;
@@ -41,29 +55,18 @@ void test(string &stri, string &stro)
     }
     file.close();
 }
-int main()
-{
-    string stri, stro;
-    // readf(stri, stro);
-    // check_wr(stri);
-    // check_wr(stro);
-    stri = "/Users/artemgudzenko/Desktop/Less_1/c++/test_NPO/input";
-    stro = "/Users/artemgudzenko/Desktop/Less_1/c++/test_NPO/ouutput";
-    test(stri, stro);
-}
 
 string inspect(string &str)
 {
     string res;
-    
+
     string tmshot;
     unsigned short int years = (stoi(str.substr(0, 4)) - 1900);                            // кол-во дней с 1900 года
     unsigned int time_m = (stoi(str.substr(14, 2)) * 60 + stoi(str.substr(17, 2))) * 1000; // количесвто миллисекунд с начала суток
 
-
     size_t pos = str.find("1ACFFC1D");
     tmshot = str.find("1ACFFC1D") != string::npos ? str.substr(pos + 8) : str.substr(25);
-    //TM кадр
+    // TM кадр
     res += to_string(years) + ' ' + to_string(time_m) + ' ' + tmshot;
     return res;
 }
